ef1b2032ae8b9a1f254ac8552f7c6d9a8b2f2d25
Malarkodi <malarkodi.m@gmail.com>
2022-05-08 23:25:35 +0530

    Add files via upload

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Product} from './product.model';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+   private apiURL = "https://catalog-api-gg-c.herokuapp.com/api";
+  //private apiURL = "https://ev5uwiczj6.execute-api.eu-central-1.amazonaws.com/test/supply-chain";
+  putErrSub = new Subject<string>();
+  public firstPage: string ="";
+  public prevPage: string ="";
+  public nextPage: string ="";
+  public lastPage: string ="";
+
+  product: Product;
+  constructor(private http: HttpClient) { }
+
+  // gets list of all products from the api
+  getAllProducts():Observable<Product[]>
+  {
+    return this.http.get<Product[]>("https://catalog-api-gg-c.herokuapp.com/api/products");
+  }
+  
+
+  //To get specific resource
+  getProductById(id: number) {       
+    return this.http.get<Product>(`${this.apiURL}/products/${id}`);
+  }
+
+  updateProduct(id: number, product: ProductForm) {   
+    return this.http.put(`${this.apiURL}/products/${id}/category/${product.category.id}`,product)
+    .subscribe(response => {     
+    }, error => {
+      this.putErrSub.next(error);     
+    });
+  
+  }
+  
+   deleteCategory(id: number) {
+     return this.http.delete(`${this.apiURL}/category/${id}`);
+  }
+  
+
+  //adds a product under a given category
+  addProduct(productInfo: ProductForm, categoryID:number)
+  {
+    let catID = categoryID; 
+    return this.http.post<Product>(`${this.apiURL}/products/category/${catID}`, productInfo)
+    // .pipe(catchError(this.handleError('addProduct', productInfo)));
+  }
+  
+  
+  //deletes a product
+  deleteProduct(productID:number){ 
+    console.log(productID);
+    
+    return this.http.delete(`${this.apiURL}/products/${productID}`);
+  }
+}
+
+export class ProductForm {
+  category: {
+    id: number;
+  }
+  name:string;
+  quantity: number;
+  price: number;
+  imageUrl: string;
+}
+
+
+
+
